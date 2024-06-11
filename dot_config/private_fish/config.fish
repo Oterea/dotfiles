d2fb1b1e96a66f72bf7cd91ba4b42fd4615db646
Oterea <zhuocli@163.com>
2024-06-11 22:16:06 +0800

    update fish with chezmoiin alias

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+function chezmoiin
+    cd ~/.local/share/chezmoi
+end
 
 
 set -gx PATH /opt/homebrew/bin $PATH
